421e45ee2de3822bd776050d11cde9ab1029aa8c
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2020-10-26 11:32:12 -0600

    Primer commit

---
+++ app/src/main/java/com/example/practica2_calculadora/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.practica2_calculadora;
+
+import android.annotation.SuppressLint;
+import android.sax.TextElementListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+}
